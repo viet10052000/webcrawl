fc7a72d836c446fd638c6a440a42e5b2396b3fe5
vietnguyen <nguyencongvieth@gmail.com>
2023-04-24 20:38:52 +0700

    crawl web

--- webcrawl/routes.py
+++ webcrawl/routes.py
@@ -8,7 +8,7 @@ DEFAULT_REQUEST_HEADERS = {
    "Accept-Language": "en",
    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36"
 }
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 
 async def get_crawl(crawl,linkstore):
     async with aiohttp.ClientSession() as session:
@@ -29,21 +29,59 @@ async def get_crawl(crawl,linkstore):
                     results.append(item)
             return results
 
+async def get_crawl_category():
+    async with aiohttp.ClientSession() as session:
+        async with session.get('https://cellphones.com.vn', headers=DEFAULT_REQUEST_HEADERS) as response:
+            html = await response.text()
+            sel = Selector(text=html)
+            datas = sel.response.css('.menu-tree>a')
+            results = []
+            for data in datas:
+                item = {
+                    'name': data.css('a>.label-item>span::text').get(),
+                    'link_url': data.css('a::attr(href)').get(),
+                }
+                
+                if item['link_url'] != '#':
+                    results.append(item)
+            return results
+
 async def get_crawl_product_detail(crawl):
     async with aiohttp.ClientSession() as session:
-        async with session.get(crawl, headers=DEFAULT_REQUEST_HEADERS) as response:
+        async with session.get(crawl['link_url'], headers=DEFAULT_REQUEST_HEADERS) as response:
             html = await response.text()
             sel = Selector(text=html)
             datas = sel.response.css('.block-detail-product')
             for data in datas:
                 price = data.css('.product__price--show::text').get()
                 item = {
+                    'product_id': crawl['_id'],
                     'price': ''.join(re.findall(r'\d+', price)),
                     'link_image': data.css('.box-ksp>img::attr(src)').get(),
                     'rating': data.css('.boxReview-score>p::text').get(),
+                    'total_rating': data.css('.boxReview-score>p>strong::text').get()
                     # 'description': data.css('.technical-content').get()
                 }
                 return item
+                   
+        await asyncio.sleep(DOWNLOAD_DELAY)
+        
+async def get_crawl_product_comment(crawl):
+    async with aiohttp.ClientSession() as session:
+        str = crawl['link_url'].replace(".html", "") + '/review'
+        async with session.get(str, headers=DEFAULT_REQUEST_HEADERS) as response:
+            html = await response.text()
+            sel = Selector(text=html)
+            datas = sel.response.css('.boxReview-comment>.boxReview-comment-item')
+            results = []
+            for data in datas:
+                item = {
+                    'product_id': crawl['_id'],
+                    'comment': data.css('.comment-content>p::text').get(),
+                }
+                results.append(item)
+                
+            return results
                    
         await asyncio.sleep(DOWNLOAD_DELAY)
 
@@ -53,6 +91,34 @@ async def get_crawl_product_detail(crawl):
 def crawl(id):
     crawl = db.crawlproducts.find_one({'_id': id})
     return render_template('admin/crawl/crawl.html',crawl=crawl)
+
+@app.route('/crawl/category', methods=['GET','POST'])
+@login_required
+@roles_required('admin')
+def crawlcategory():
+    if request.method == 'GET':
+        return render_template('admin/crawl/crawlcategory.html')
+
+@app.route('/crawl/category/show', methods=['GET','POST'])
+@login_required
+@roles_required('admin')
+def crawlcategory_show():
+    if request.method == 'POST':
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+        data = loop.run_until_complete(get_crawl_category())
+        new_list = []
+
+        [new_list.append(item) for item in data if item not in new_list]
+        
+        if os.path.exists('data.json'):
+            with open('data.json', 'w') as outfile:
+                json.dump({}, outfile)
+
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
+            
+        return render_template('admin/crawl/crawlcategoryshow.html',data=new_list)
 
 @app.route('/crawl/show/<id>', methods=['POST'])
 @login_required
@@ -80,22 +146,49 @@ def crawlshow(id):
         #     return data
         return render_template('admin/crawl/showcrawl.html',data=data,crawlproduct=crawlproduct,store=store,category=category)
    
-@app.route('/crawl/detail', methods=['GET','POST'])
+@app.route('/crawl/detail', methods=['GET'])
 @login_required
 @roles_required('admin')
 def crawldetail():
     if request.method == 'GET':
         return render_template('admin/crawl/crawldetail.html')
-    elif request.method == 'POST':
+    
+@app.route('/crawl/comment', methods=['GET'])
+@login_required
+@roles_required('admin')
+def crawlcomment():
+    if request.method == 'GET':
+        return render_template('admin/crawl/crawlcomment.html')
+
+@app.route('/crawl/detail/show', methods=['POST'])
+@login_required
+@roles_required('admin')
+def crawldetail_show():
+    if request.method == 'POST':
         store = db.stores.find_one({'name': 'cellphones'})
         data = db.products.find({'store_id': store['_id']})
         datas = []
         for item in data:
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
-            result = loop.run_until_complete(get_crawl_product_detail(item['link_url']))
+            result = loop.run_until_complete(get_crawl_product_detail(item))
             datas.append(result)
-        return jsonify(datas)   
+        return render_template('admin/crawl/crawldetailshow.html',data=datas)
+
+@app.route('/crawl/comment/show', methods=['POST'])
+@login_required
+@roles_required('admin')
+def crawlcomment_show():
+    if request.method == 'POST':
+        store = db.stores.find_one({'name': 'cellphones'})
+        data = db.products.find({'store_id': store['_id']})
+        datas = []
+        for item in data:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            result = loop.run_until_complete(get_crawl_product_comment(item))
+            datas.append(result)
+        return render_template('admin/crawl/crawlcommentshow.html',data=datas)
 
 @app.route('/crawl/save', methods=['POST'])
 @login_required
