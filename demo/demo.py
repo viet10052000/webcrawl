a253ae2149ee0695a916db285b1beb895ff2f74d
viet10052000 <nguyencongvieth@gmail.com>
2023-06-07 10:24:17 +0700

    fix bug code

--- demo/demo.py
+++ demo/demo.py
@@ -4,10 +4,45 @@ import pymongo
 # Kết nối tới MongoDB
 client = pymongo.MongoClient('mongodb+srv://user:123456Aa@cluster0.t3aqomt.mongodb.net')
 db = client['shops']
-collection = db['products']
-categories = db['categories']
-# Dữ liệu ban đầu
-import re
-products = list(collection.find())
-categories = list(db.categories.find({},{'_id':1,'name':1}))
-print(categories)
+
+categories = db.categories.find_one({"name": "iphone"},{'_id':1,'name':1})
+products = list(db.products.find({"category_id": categories["_id"]},{"name":1}))
+list_ip = []
+for item in products:
+  list_ip.append(item["name"])
+
+iphone_list = [
+    "iPhone 14 Pro Max 128GB",
+    "iPhone 14 Pro Max 512GB",
+    "iPhone 14 Pro Max 256GB",
+    "iPhone 14 Pro 128GB",
+    "iPhone 14 Pro 256GB",
+    "iPhone 14 Pro 512GB",
+    "iPhone 14 128GB",
+    "iPhone 14 256GB",
+    "iPhone 14 Plus 128GB",
+    "iPhone 14 Plus 256GB",
+    "iPhone 14 Plus 512GB",
+    "iPhone 13 128GB",
+    "iPhone 12 64GB",
+    "iPhone 12 128GB",
+    "iPhone 11 64GB",
+    "iPhone 11 128GB",
+    "iPhone SE (2022) 128GB",
+    "iPhone SE (2022) 64GB"
+]
+
+grouped_iphones = {}
+
+for iphone in iphone_list:
+    name = " ".join(iphone.split()[:-1])  # Lấy tên iPhone bằng cách loại bỏ thông tin về dung lượng
+    if name in grouped_iphones:
+        grouped_iphones[name].append(iphone)
+    else:
+        grouped_iphones[name] = [iphone]
+
+# # In kết quả
+# for name, models in grouped_iphones.items():
+#     print(name + ":")
+#     for model in models:
+#         print("  - " + model)
--- user/routes.py
+++ user/routes.py
@@ -29,6 +29,13 @@ def userdelete(id):
     lists = User().delete(id)
     return redirect('/user/list')
 
+@app.route('/profile/admin', methods=['GET'])
+@login_required
+@roles_required('admin','collector')
+def profile_admin():
+    user = db.users.find_one({'_id': session['user']['_id']})
+    return render_template('adminv2/user/profile.html',user=user)
+
 @app.route('/profile', methods=['GET'])
 @login_required
 def profile():
@@ -51,6 +58,43 @@ def profileEdit():
         }
         db.users.update_one({ '_id': user['_id'] }, { '$set': data })
         return redirect('/profile')
+
+@app.route('/changepassword/admin', methods=['GET','POST'])
+@login_required
+@roles_required('admin','collector')
+def changePassword_admin():
+    if request.method == 'GET':
+        return render_template('adminv2/user/changepass.html',category=g.categories)
+    elif request.method == 'POST':
+        user = db.users.find_one({'_id': session['user']['_id']})
+        checkpass = pbkdf2_sha256.verify(request.values.get('old_password'), user['password'])
+        if checkpass == False:
+            data = {
+                "error":"Mật khẩu không chính xác"
+            }
+            return render_template('adminv2/user/changepass.html',user=data)
+        else:
+            if request.values.get('old_password') == request.values.get('new_password'):
+                data = {
+                    "error":"Mật khẩu không được trùng"
+                }
+                return render_template('adminv2/user/changepass.html',user=data)
+            elif request.values.get('new_password') != request.values.get('confirm_password'):
+                data = {
+                    "error":"Mật khẩu xác nhận không chính xác"
+                }
+                return render_template('user/auth/changepassword.html',user=data) 
+            else:
+                data = {
+                    'old_password': request.values.get('old_password'),
+                    'new_password': request.values.get('new_password'),
+                    'confirm_password': request.values.get('confirm_password'),
+                }
+                password = pbkdf2_sha256.encrypt(data['new_password'])
+                user["password"] = password
+                db.users.update_one({ '_id': user['_id'] }, { '$set': user })
+                Auth().signout()
+                return redirect('/login')
     
 @app.route('/changepassword', methods=['GET','POST'])
 @login_required
